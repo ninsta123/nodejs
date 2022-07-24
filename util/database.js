05eecece88e65ed2f3c3cb3661a0a4e55813d5da
ninsta123 <63466014+ninsta123@users.noreply.github.com>
2022-07-24 18:52:49 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -2,6 +2,6 @@ const Sequelize =  require("sequelize");
 
 const sequelize =  new Sequelize("School", "myserver", "nishtha!123A" ,{
     dialect:"mysql",
-    host:"myserver3456.mysql.database.azure.comSS"
+    host:"myserver3456.mysql.database.azure.com"
 })
 module.exports =  sequelize;
